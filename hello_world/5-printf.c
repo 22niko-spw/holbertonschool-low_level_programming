d73b286b1d57d2e0f88116f1b63a04a8fdfa043a
22niko-spw <nicosojd@gmail.com>
2026-02-23 13:01:02 +0100

    feat: display specific string using puts

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 /**
- * Print a sentence with printf
+ * main - Point d'entrée du programme
+ *
+ * Description : Affiche une phrase spécifique sur la grammaire et l'art
+ * en utilisant printf, tout en respectant les standards de compilation.
+ *
+ * Return: Toujours 0 (Succès)
  */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
