43112912ce14651dc731a11a93c3269f0228c71f
22niko-spw <nicosojd@gmail.com>
2026-02-23 13:33:57 +0100

    feat: print the size of various types on the computer

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Point d'entrée
+ *
+ * Description : Affiche la taille de divers types de données
+ * (char, int, long int, long long int, float) sur la machine locale.
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+
+	return (0);
+}
