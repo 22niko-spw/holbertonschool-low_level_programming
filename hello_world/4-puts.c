9bbe58dd79aa763281cb05477c9ae5275ac99af2
22niko-spw <nicosojd@gmail.com>
2026-02-23 12:49:23 +0100

    feat: display specific string using puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description : Ce programme affiche exactement la phrase
+ * "Programming is like building a multilingual puzzle
+ * suivie d'une nouvelle ligne en utilisant la fonction puts.
+ *
+ * Return: Toujours 0 (indique que le programme s'est terminé avec succès)
+ */
+int main (void)
+
+{puts ("Programming is like building a multilingual puzzle");
+    return (0);
+}
