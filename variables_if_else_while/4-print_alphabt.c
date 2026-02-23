3a578edad303b07b022b482dfa32b6fb1b3a0005
22niko-spw <nicosojd@gmail.com>
2026-02-23 16:49:55 +0100

    Print alphabet except e and q using putchar 2 times

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Point d'entrée
+ *
+ * Description: Affiche l'alphabet en minuscules.
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar (letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
