2ce7348463153d5fdb10c9c0d6d0b68fb2104f54
22niko-spw <nicosojd@gmail.com>
2026-02-23 16:35:28 +0100

    Print alphabet in lowercase and uppercase using putchar 3 times

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Point d'entrée
+ *
+ * Description: Affiche l'alphabet en minuscules.
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		putchar (letter);
+	}
+	for (letter = 'A' ; letter <= 'Z' ; letter++)
+	{
+		putchar (letter);
+	}
+	putchar('\n');
+
+
+	return (0);
+}
