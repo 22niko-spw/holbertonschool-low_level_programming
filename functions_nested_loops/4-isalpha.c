4f76f1650a52c3cef96ac95df0ae28297c249fbe
22niko-spw <nicosojd@gmail.com>
2026-02-24 19:37:12 +0100

    feat: add _isalpha function

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - Vérifie si un caractère est alphabétique.
+ * @c: Le caractère à vérifier (sous forme d'entier).
+ *
+ * Return: 1 si c est une lettre (majuscule ou minuscule), 0 sinon.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') | (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
