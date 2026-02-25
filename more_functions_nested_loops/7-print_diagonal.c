fc3c5212bed6fb24cf3b93938d50384860310a4f
22niko-spw <nicosojd@gmail.com>
2026-02-25 19:47:19 +0100

    Add a function that print lines in diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal.
+ * @n: The number of times the character \ should be printed.
+ *
+ * Description: Uses _putchar to print spaces and backslashes.
+ */
+void print_diagonal(int n)
+{
+	int row, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 0; row < n; row++)
+		{
+			for (space = 0; space < row; space++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
