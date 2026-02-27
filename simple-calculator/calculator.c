1eecf85641f71377521b474e0dbd65faea95e1aa
22niko-spw <nicosojd@gmail.com>
2026-02-27 13:57:31 +0100

    Add Menu of Calculator

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point for the calculator program.
+ * * Return: 0 if the program exits successfully,
+ * otherwise a non-zero error code.
+ */
+int main(void)
+{
+	int choice;
+
+	printf("Simple Calculator\n");
+	printf("1) ADD\n");
+	printf("2) Substract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+
+	scanf("%d", &choice);
+
+	if (choice == 0)
+	{
+		printf("Bye!\n");
+	}
+		return (0);
+}
