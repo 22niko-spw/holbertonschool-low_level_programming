3bccb9c05e4a7989fc6181a2dc144c0f8f0c9aa1
22niko-spw <nicosojd@gmail.com>
2026-03-05 15:10:36 +0100

    Fix the code

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _strchr - localise un caractère dans une chaîne
  * @s: la chaîne de caractères à parcourir
@@ -12,19 +13,15 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	/* On parcourt la chaîne jusqu'au caractère nul '\0' inclus */
 	while (s[i])
 	{
 		if (s[i] == c)
-		{
-			/* On retourne l'adresse de l'endroit où on a trouvé c */
 			return (s + i);
-		}
-		/* Si on vient de tester le caractère de fin de chaîne, on s'arrête */
-		if (s[i] == '\0')
-			break;
 		i++;
 	}
+
+	if (s[i] == c)
+		return (s + i);
 
 	return (NULL);
 }
