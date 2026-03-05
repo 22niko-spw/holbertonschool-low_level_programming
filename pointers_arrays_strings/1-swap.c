d444f7d0eec62fd7f4a11255be6a85366b2e546b
22niko-spw <nicosojd@gmail.com>
2026-03-05 13:29:56 +0100

    Add comment to the code

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * swap_int - inverse la valeur de deux entiers.
+ * @a: pointeur vers le premier entier.
+ * @b: pointeur vers le second entier.
+ *
+ * Return: void (ne retourne rien).
+ */
 void swap_int(int *a, int *b)
 {
 	int temp = *a;
