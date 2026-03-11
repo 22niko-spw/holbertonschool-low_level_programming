1c3d92597e96336f960dedbbc39a7abbc704991b
22niko-spw <nicosojd@gmail.com>
2026-03-11 11:18:41 +0100

    Add Comment Betty

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: variable
+ *
+ * Return: result
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
