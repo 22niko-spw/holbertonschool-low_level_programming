59f9c4dcd331e9c3229408e50e3b87b3e2d3597a
22niko-spw <nicosojd@gmail.com>
2026-03-16 16:10:30 +0100

    Add a function that search for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,25 +1,27 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * int_index - searches for an integer
- * @array: tableau d'entier
- * @size: taille du tableau
- * @cmp: pointeur vers la fonction pour comparer les valeurs
- * Return: index first element for cmp return 0, or -1 if not found
+ * int_index - Searches for an integer in an array.
+ * @array: The array to search in.
+ * @size: The number of elements in the array.
+ * @cmp: A pointer to the function to be used to compare values.
+ *
+ * Return: Index of the first element for which cmp doesn't return 0.
+ * If no element matches or size <= 0, return -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
 	if (size <= 0)
 		return (-1);
-
-	while (i < size)
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
-		i++;
 	}
-
 	return (-1);
-
 }
+
