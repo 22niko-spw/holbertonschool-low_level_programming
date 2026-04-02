25c1adea5d874f0aa22c0ddc6c54345ff6950c90
22niko-spw <nicosojd@gmail.com>
2026-04-02 23:44:21 +0200

    feat: Add a function that adds an element to the hash table.

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Ajoute ou met à jour un élément dans la table de hachage
+ * @ht: La table de hachage
+ * @key: La clé (ne peut pas être une chaîne vide)
+ * @value: La valeur associée à la clé (dupliquée, peut être vide)
+ *
+ * Return: 1 si succès, 0 sinon
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	hash_node_t *tmp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
